6e76a5f945eed16ce2550ed74dd9edfd757c831a
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-05-22 12:24:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# promise-memoize
+
+[![Build Status](https://img.shields.io/travis/nodeca/promise-memoize/master.svg?style=flat)](https://travis-ci.org/nodeca/promise-memoize)
+[![NPM version](https://img.shields.io/npm/v/promise-memoize.svg?style=flat)](https://www.npmjs.org/package/promise-memoize)
+
+> Memoize promise-returning functions. Includes cache expire and prefetch.
+
+- When data expire mode enabled, new values are fetched in advance. Cache
+  will be always valid, without "gaps".
+  - Prefetch happens only for items in use. Inactive ones will be GC-ed as usual.
+- Errors are not cached
+  - You still can enable cache with separate expire time for errors, to avoid
+    specific peak loads. For example, set 120s for good result and 1s on fail.
+
+
+## Install
+
+```bash
+npm install promise-memoize --save
+```
+
+
+## Usage example
+
+```js
+// Pseudo code
+let db = require('mongoose').createConnection('mongodb://localhost/forum');
+
+function lastPosts(limit) {
+  return db.model('Post').find().limit(limit).orderBy('-_id').lean(true).exec(); // <- Promise
+}
+
+let cachedLastPosts = require('promise-memoize')(lastPosts, { maxAge: 60000 });
+
+// Later...
+cachedLastPosts(10).then(posts => console.log(posts));
+```
+
+## API
+
+### promiseMemoize(fn [, options]) -> memoizedFn
+
+Memoize function `fn`.
+
+ - **fn(params...)** — function, returning a promise (or any "thenable").
+   It can have any number of arguments, but arguments should be uniquely
+   castable to strings (see below).
+ - **options** — options for memoization (optional)
+   - **maxAge** — an amount of milliseconds it should cache resolved
+     values for (default: `Infinity`, i.e. cache forever).
+   - **maxErrorAge** — an amount of milliseconds it should cache
+     rejected values for (default: `0`, i.e. don't cache).
+   - **resolve** — serialiser to build unique key from `fn` arguments.
+     (default: `simple`). Possible values:
+     - `simple` (string) — convert each param to string & join those.
+     - `json` (string) — JSON.stringify each param & join results.
+     - function(Array) — custom function, with `fn` params as array on input
+     - `[ String, Boolean, 'json', function ]` — array with custom functions,
+       specific for each `fn` param position (text shortcuts as above are allowed).
+
+Return value is a function with the same signature as *fn*.
+
+**Note. How prefetch works.**
+
+If `maxAge` used and request to cached data happens after `0.7 * maxAge` time, then:
+
+- cached data returned
+- `fn` call is executed in parallel
+- cached data will be substituted with new one on success, timeouts will be extended.
+
+So your application will not have to wait for data fetch after cache expire.
+
+
+### memoizedFn(params...) -> promise
+
+Returns result as cached promise (errors are not cached by default). If `maxAge`
+used, tries to prefetch new value before expire to replace cache transparently.
+
+
+### memoizedFn.clear()
+
+Remove all cached data.
+
+
+## License
+
+[MIT](https://github.com/nodeca/promise-memoize/blob/master/LICENSE)
---
+++ test/test_memoize.js
@@ -0,0 +1,138 @@
+/*eslint-env es6, node, mocha */
+
+'use strict';
+
+var assert  = require('assert');
+var co      = require('co');
+var memoize = require('../');
+
+
+describe('promise-memoize', function () {
+  function counter() {
+    return new Promise(resolve => {
+      process.nextTick(() => resolve(counter.value++));
+    });
+  }
+
+  function rejecter() {
+    return new Promise((resolve, reject) => {
+      process.nextTick(() => reject(rejecter.value++));
+    });
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(), ms);
+    });
+  }
+
+  beforeEach(function () {
+    counter.value = 0;
+    rejecter.value = 0;
+  });
+
+  it('should memoize functions', co.wrap(function* () {
+    let memoized = memoize(counter);
+
+    assert.equal(yield memoized(1, 2), 0, 'assert A #1');
+    assert.equal(yield memoized(1, 2), 0, 'assert A #2');
+    assert.equal(yield memoized(3, 4), 1, 'assert B #1');
+    assert.equal(yield memoized(1, 2), 0, 'assert A #3');
+    assert.equal(yield memoized(3, 4), 1, 'assert B #2');
+    assert.equal(yield memoized(),     2, 'assert C #1');
+  }));
+
+  it('should not cache errors by default', co.wrap(function* () {
+    let memoized = memoize(rejecter);
+    let i = 0;
+
+    try { yield memoized(1, 2); } catch (err) { i++; assert.equal(err, 0, 'assert A #1'); }
+    try { yield memoized(1, 2); } catch (err) { i++; assert.equal(err, 1, 'assert A #3'); }
+
+    assert.equal(i, 2);
+  }));
+
+  it('should accept complex arguments', co.wrap(function* () {
+    let memoized = memoize(counter, { resolve: 'json' });
+
+    assert.equal(yield memoized({ x: 1 }, [ 'foo' ]), 0, 'assert A #1');
+    assert.equal(yield memoized({ x: 1 }, [ 'foo' ]), 0, 'assert A #2');
+    assert.equal(yield memoized({ x: 2 }, [ 'foo' ]), 1, 'assert B #1');
+    assert.equal(yield memoized({ x: 1 }, [ 'bar' ]), 2, 'assert C #1');
+    assert.equal(yield memoized({ x: 1 }, [ 'foo' ]), 0, 'assert A #3');
+    assert.equal(yield memoized({ x: 2 }, [ 'foo' ]), 1, 'assert B #2');
+  }));
+
+  it('should be reset on .clear()', co.wrap(function* () {
+    let memoized = memoize(counter);
+
+    assert.equal(yield memoized(1, 2), 0, 'assert A #1');
+    assert.equal(yield memoized(1, 2), 0, 'assert A #2');
+    memoized.clear();
+    assert.equal(yield memoized(1, 2), 1, 'assert A #3');
+  }));
+
+  it('should respect maxAge', co.wrap(function* () {
+    let memoized = memoize(counter, { maxAge: 10 });
+
+    assert.equal(yield memoized(123), 0, 'assert A #1');
+    assert.equal(yield memoized(123), 0, 'assert A #2');
+    yield sleep(50);
+    assert.equal(memoized.clear(), 0); // cache should expire
+    assert.equal(yield memoized(123), 1, 'assert A #3');
+    assert.equal(yield memoized(123), 1, 'assert A #4');
+    assert.equal(memoized.clear(), 1); // cache should be dirty
+  }));
+
+  it('should respect maxErrorAge', co.wrap(function* () {
+    let memoized = memoize(rejecter, { maxErrorAge: 10 });
+    let i = 0;
+
+    try { yield memoized(1, 2); } catch (err) { i++; assert.equal(err, 0, 'assert A #1'); }
+    try { yield memoized(1, 2); } catch (err) { i++; assert.equal(err, 0, 'assert A #2'); }
+    yield sleep(15);
+    try { yield memoized(1, 2); } catch (err) { i++; assert.equal(err, 1, 'assert A #3'); }
+    try { yield memoized(1, 2); } catch (err) { i++; assert.equal(err, 1, 'assert A #4'); }
+
+    assert.equal(i, 4);
+  }));
+
+  it('should accept resolve function', co.wrap(function* () {
+    let memoized = memoize(counter, {
+      resolve: args => args[0].x + args[1][0]
+    });
+
+    assert.equal(yield memoized({ x: 1 }, [ 1, 2, 3 ]), 0, 'assert A #1');
+    assert.equal(yield memoized({ x: 2 }, [ 1, 2, 3 ]), 1, 'assert B #1');
+    assert.equal(yield memoized({ x: 1 }, [ 2, 2, 3 ]), 1, 'assert C #1');
+    assert.equal(yield memoized({ x: 1 }, [ 1, 4, 5 ]), 0, 'assert A #2');
+  }));
+
+  it('should run prefetch', co.wrap(function* () {
+    let memoized = memoize(counter, { maxAge: 100 });
+
+    assert.equal(yield memoized(123), 0, 'assert #1');
+    assert.equal(yield memoized(123), 0, 'assert #2');
+
+    yield sleep(40);
+    assert.equal(yield memoized(123), 0, 'assert #3');
+    assert.equal(yield memoized(123), 0, 'assert #4');
+    assert.equal(counter.value, 1);
+
+    yield sleep(40); // 80+ msec, prefetch should run here
+    assert.equal(yield memoized(123), 0, 'assert #5');
+    assert.equal(counter.value, 1); // still 1, 'cause updating next tick
+
+    yield sleep(40); // 120+ msec
+    assert.equal(counter.value, 2);
+    assert.equal(yield memoized(123), 1, 'assert #6');
+    assert.equal(yield memoized(123), 1, 'assert #7');
+    assert.equal(counter.value, 2);
+  }));
+
+  it('should throw on wrong resolver', function () {
+    assert.throws(function () {
+      memoize(counter, { resolve: 'foo' });
+    });
+  });
+});
